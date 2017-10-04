7d20a33456efcafe425590142c633022f5d4507b
mtlubchain <mtlubchainu@hotmail.com>
2017-10-04 15:23:04 +0200

    before webpack commit

--- src/blockModel.js
+++ src/blockModel.js
@@ -7,6 +7,7 @@ class BlockModel {
 			this.hash = data.hash;
 		}
 }
+module.exports = BlockModel;
 
 
 
--- src/blockService.js
+++ src/blockService.js
@@ -1,12 +1,12 @@
 (function (app) {
     // const request = require('request');
     // const BlockModel = app.models.BlockModel;
-    const BlockModel = require('./blockModel0.js');
+    // const BlockModel = require('./blockModel.js');
     class BlockService {
         constructor() {
             console.log('hello from BlockService');
-            this.block = new BlockModel(0,"dupa0","dzisiaj","oto moje dane","smaz hasz");
-            console.log(block);
+        //     this.block = new BlockModel(0,"dupa0","dzisiaj","oto moje dane","smaz hasz");
+        //     console.log(block);
         }
     }
     app.services.BlockService = BlockService;
